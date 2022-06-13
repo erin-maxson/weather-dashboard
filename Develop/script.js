93c2867d56b6e04775459dbeeded759f8e50991d
erin-maxson <erin@cohatch.com>
2022-06-13 15:53:34 -0700

    adding second api call for 5 day forecast

--- Develop/script.js
+++ Develop/script.js
@@ -13,15 +13,26 @@ var uvEl = document.querySelector("#uv")
 var cityDay1 = document.querySelector("#city-header-card-1")
 
 // API Key
-var api = " "
+var api = "4361959541047617a16f6a917f159060"
 
 
 // function to display weather
 
-function displayWeather {
+function displayWeather() {
+    event.preventDefault()
     var cityName = cityEl.value
-    var urlCurrentWeather = 'https://api.openweathermap.org/data/2.5/weather?q={cityName}&appid={api}'
+    var urlCurrentWeather = 'https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${api}'
 }
+
+fetch (urlCurrentWeather)
+.then(function(response){
+    return response.json()
+})
+
+.then(function (currentData) {
+    console.log(currentData)
+    var fiveDayWeather = 'https://api.openweathermap.org/data/2.5/onecall?lat=${currentData.coord.lat}&lon=${currentData.coord.lon}&exclude={part}&appid=${api}'
+})
 
 
 // addEventListener on submit and create a dashboard function
