c2010a3ad3ab4a2cea8234dc866cde45685c662c
erin-maxson <erin@cohatch.com>
2022-06-13 15:42:16 -0700

    adding variables for city card info + 5 day cast

--- Develop/script.js
+++ Develop/script.js
@@ -2,8 +2,15 @@
 var cityEl = document.querySelector("#citySearchText")
 var cityFormEl = document.querySelector("#city-form")
 
+// city big card info
+var cityHeaderEl = document.querySelector("#city-header")
+var tempEl = document.querySelector("#temp")
+var windEl = document.querySelector("#wind")
+var humidEl = document.querySelector("#humidity")
+var uvEl = document.querySelector("#uv")
 
+// city 5 day forecast cards
+var cityDay1 = document.querySelector("#city-header-card-1")
 
-
-
+// addEventListener on submit and create a dashboard function
 
