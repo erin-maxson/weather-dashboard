f7c9686ed879cae7a66c058b039590558674b343
erin-maxson <erin@cohatch.com>
2022-06-28 15:59:11 -0700

    working on getting weather points to change main

--- Develop/script.js
+++ Develop/script.js
@@ -11,17 +11,21 @@ var uvEl = document.querySelector("#uv")
 
 // city 5 day forecast cards
 var cityDay1 = document.querySelector("#city-header-card-1")
+var cardTemp = document.querySelector("#tempCard")
+var cardWind = document.querySelector("#windCard")
+var cardHumidity = document.querySelector("#humidityCard")
+var cardUV = document.querySelector("#uvCard")
 
 // API Key
-var api = "4361959541047617a16f6a917f159060"
+var api = 'b9ed88a608928d0800d199dca396e9ad'
 
 
-// function to display weather
+// function to display weather to big card
 
 function displayWeather() {
     event.preventDefault()
     var cityName = cityEl.value
-    var urlCurrentWeather = 'https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${api}'
+    var urlCurrentWeather = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${api}`
 
     fetch(urlCurrentWeather)
         .then(function (response) {
@@ -30,7 +34,7 @@ function displayWeather() {
 
         .then(function (currentData) {
             console.log(currentData)
-            var fiveDayWeather = 'https://api.openweathermap.org/data/2.5/onecall?lat=${currentData.coord.lat}&lon=${currentData.coord.lon}&exclude={part}&appid=${api}'
+            var fiveDayWeather = `https://api.openweathermap.org/data/2.5/onecall?lat=${currentData.coord.lat}&lon=${currentData.coord.lon}&exclude={part}&appid=${api}`
 
             fetch(fiveDayWeather)
                 .then(function (response) {
@@ -44,17 +48,48 @@ function displayWeather() {
 
                     cityHeaderEl.textContent = currentData.name + " " + currentDate
                     var iconImage = document.createElement("img")
-                    iconImage.setAttribute("src", 'http://openweathermap.org/img/wn/${currentData.weather[0].icon}@2x.png')
+                    iconImage.setAttribute("src", `http://openweathermap.org/img/wn/${currentData.weather[0].icon}@2x.png`)
                     tempEl.innerHTML = currentData.main.temp
-                    // windEl.innerHTML = currentData
-                    // humidEl.innerHTML = currentData.
-                    // uvEl.innerHTML = currentData.
-
-
+                    console.log(tempEl.innerHTML)
+                    windEl.innerHTML = currentData.wind.speed
+                    console.log(windEl.innerHTML)
+                    humidEl.innerHTML = currentData.main.humidity
+                    console.log(humidEl.innerHTML)
+                    uvEl.innerHTML = fiveDayData.current.uvi
+                    console.log(uvEl.innerHTML)
                 })
 
         })
+ 
+    };
+    
+    function updateWeatherCard() {
+        event.preventDefault()
+        var cityName = cityDay1.value
+        var urlFiveDayWeather = `https://api.openweathermap.org/data/2.5/forecast?lat=${currentData.coord.lat}&lon=${currentData.coord.lon}&appid=${api}`
 
-    }   
+
+        fetch(urlFiveDayWeather)
+        .then(function (response) {
+            return response.json()
+        })
+
+        .then(function (updateWeatherCard) {
+            console.log(updateWeatherCard)
+
+            var currentDate = moment.unix(currentData.dt).format("MM/DD/YYYY")
+
+            cityDay1.textContent = currentData.name + " " + currentDate
+            var iconImage = document.createElement("img")
+            iconImage.setAttribute("src", `http://openweathermap.org/img/wn/${currentData.weather[0].icon}@2x.png`)
+            cardTemp.innerHTML = currentData.main.temp
+            cardWind.innerHTML = currentData.wind.speed
+            cardHumidity.innerHTML = currentData.main.humidity
+            cardUV.innerHTML = fiveDayWeather.current.uvi
+        })
+    }
+
+
+
     // addEventListener on submit and create a dashboard function
     cityFormEl.addEventListener("submit", displayWeather)
